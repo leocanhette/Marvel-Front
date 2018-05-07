c34702be779118bf8279b3d38e21db894ab5d013
Leo Canhette (Concrete) <leonardo.canhette@concrete.com.br>
2018-05-07 16:38:30 -0300

    Adjusted redirect http

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ var app = express();
 
 var PORT = process.env.PORT;
 
+app.use(function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
+
 app.use(express.static('Public'));
 
 app.listen(PORT, function () {
